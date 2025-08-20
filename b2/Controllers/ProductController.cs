5f6247371819e767464092133cf35a71f6268460
pdnam2003 <nam.pd.2446@aptechlearning.edu.vn>
2025-08-20 23:14:04 +0700

    b2

---
+++ b2/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using b2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace b2.Controllers
+{
+    public class ProductController : Controller
+    {
+        List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Product A", Price = 100 },
+            new Product { Id = 2, Name = "Product B", Price = 200 },
+            new Product { Id = 3, Name = "Product C", Price = 300 }
+        };
+        public IActionResult Index()
+        {
+            return View(products); // Sử dụng được View() vì đã kế thừa đúng Controller
+        }
+        public IActionResult Details(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+    }
+}
---
+++ b2/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace b2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+
+    }
+}
